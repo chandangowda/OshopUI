121a0ccf105e34868273997cb42b8d937842284d
Chikkabasavanahalli Baburam <Chandangowda.Chikkabasavanahallibaburam@CenturyLink.com>
2019-03-28 18:05:19 +0530

    call

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import {  Router } from '@angular/router';
+import {  map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class AuthService {
   constructor(private http: HttpClient,private cookieService: CookieService,private route:Router) { }
 
    obtainAccessToken(loginData) {
-     console.log(loginData);
+    
     let params = new URLSearchParams();
     params.append('username', loginData.username);
     params.append('password', loginData.password);
@@ -26,13 +27,30 @@ export class AuthService {
 
     this.http.post("http://35.200.215.246:8777/auth-api/oauth/token",params.toString(),options)
     .subscribe(response=>this.saveTokenData(response));
+    
 
   }
 
   saveTokenData(tokenData){
     var expireDate = new Date().getTime() + (1000 * tokenData.expires_in);
     this.cookieService.set("access_token", tokenData.access_token, expireDate);
-    console.log(this.cookieService.get("access_token"))
+    this.callGetCall();
     this.route.navigate(['/']);
+  }
+
+  callGetCall(){
+    let token=this.cookieService.get("access_token");
+    let headers = new HttpHeaders({ 'Content-type': 'application/json; charset=utf-8', 'Authorization': 'Bearer ' + token,responseType: 'text' });
+    let options: {
+      headers?: HttpHeaders,
+      responseType: 'text',
+  } = {
+      headers: headers,
+      responseType: 'text'
+  };
+
+    this.http.get('http://35.200.215.246:8777/product-api/test123',options)
+   
+    .subscribe(res =>console.log(res))
   }
 }
