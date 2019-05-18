4ac3a081a580cd7be4e7dc1bb367cdba88dcfe5b
chandangowda <chandangowdacb@gmail.com>
2019-05-19 00:04:46 +0530

    Cart Addon

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'src/model/product';
 import { ShoppingCartService } from '../service/shopping-cart.service';
 import { ShoppingCart } from 'src/model/shoppingCart';
-import { Item } from 'src/model/item';
+import {take } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'product-card',
@@ -14,24 +16,44 @@ export class ProductCardComponent implements OnInit {
   @Input('product')product:Product;
   @Input('show-actions')showActions:boolean=true;
   @Input('shoping-cart')shoppingCart:ShoppingCart;
+  countNumber:number;
 
   constructor(private cartService:ShoppingCartService) { }
 
-  addToCart(product:Product){
-    this.cartService.addToCart(product);
+  addToCart(){
+    this.cartService.addToCart(this.product).then(res=>{
+        res.pipe(take(1)).subscribe(response=>{
+          this.shoppingCart=response.cartData[0];
+          this.getQuantity();
+        })
+    });
+  }
+
+  removeFromCart(){
+    this.cartService.removeFromCart(this.product).then(res =>{
+      res.subscribe(response =>{
+        this.shoppingCart=response.cartData[0];
+        this.getQuantity();
+      })
+    });
   }
 
   getQuantity(){
-
-    if(!this.shoppingCart) return 0;
+     
+    if(!this.shoppingCart) {
+       this.countNumber=0;
+       return this.countNumber;
+     };
   
 
     for(let i=0;i<this.shoppingCart.items.length;i++){
       if(this.shoppingCart.items[i].productId === this.product.id){
-        
-        return this.shoppingCart.items[i].cartCount ?this.shoppingCart.items[i].cartCount:0;
+        this.shoppingCart.items[i].cartCount ?this.countNumber=this.shoppingCart.items[i].cartCount:this.countNumber=0;
+        return this.countNumber
       }
     }
+    this.countNumber=0;
+    return this.countNumber;
    
      
     
--- src/app/service/shopping-cart.service.ts
+++ src/app/service/shopping-cart.service.ts
@@ -4,9 +4,7 @@ import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { ShoppingCartResponse } from 'src/model/shoppingCartResponse';
 import { Product } from 'src/model/product';
-import {take } from 'rxjs/operators';
-import { switchMap } from 'rxjs/operators';
-import { from } from 'rxjs';
+
 
 
 @Injectable({
@@ -26,6 +24,22 @@ export class ShoppingCartService {
     };
 
     return this.http.post<ShoppingCartResponse>('http://localhost:8777/product-api/shoppingcart/savecart',{dateCreated:new Date().getTime()},options)
+
+  }
+
+  async removeFromCart(product:Product):Promise<Observable<ShoppingCartResponse>>{
+    console.log('remobe')
+    let cartId= await   this.getOrCreateCartId();
+    
+    let token=this.cookieService.get('access_token');  
+    let headers = new HttpHeaders({ 'Content-type': 'application/json; charset=utf-8', 'Authorization': 'Bearer ' + token });
+    let options: {
+      headers?: HttpHeaders
+    } = {
+      headers: headers
+    };
+
+    return this.http.delete<ShoppingCartResponse>('http://localhost:8777/product-api/shoppingcart/deleteProductById/'+cartId+'/'+product.id,options)
 
   }
 
@@ -75,8 +89,7 @@ export class ShoppingCartService {
         headers: headers
       };
   
-      let cartResponse=this.http.post<ShoppingCartResponse>('http://localhost:8777/product-api/shoppingcart/findbycartproid/'+cartId+'/'+product.id,{},options)
-      cartResponse.pipe(take(1)).subscribe(res=>console.log(res));
+     return this.http.post<ShoppingCartResponse>('http://localhost:8777/product-api/shoppingcart/findbycartproid/'+cartId+'/'+product.id,{},options)
     }
   
 }
